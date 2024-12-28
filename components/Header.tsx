0f5c31d41ac36db8ca48f694b0267bc891fb9210
daljo25 <camalion007@gmail.com>
2024-12-28 15:48:59 +0100

    mejora el encabezado con nuevos botones y animaciones, y actualiza la configuración de Tailwind para incluir animación de giro

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,26 @@
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from "@/components/ModeToggle"
+import { Printer, RotateCcw, Globe, LoaderPinwheel } from "lucide-react"
+import Link from "next/link"
 
 export default function Header({ onNewGame }: { onNewGame: () => void }) {
   return (
-    <header className="bg-primary text-primary-foreground shadow-md dark:bg-dark dark:text-dark-foreground">
+    <header className="bg-white text-primary shadow-md dark:bg-gray-800 dark:text-gray-400">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="text-3xl font-bold">Bingo</div>
-        <Button variant="destructive" onClick={onNewGame}>Nueva Partida</Button>
-        <ModeToggle />
+        <div className="flex items-center justify-center gap-2 text-3xl font-bold ">
+          <LoaderPinwheel className="animate-spin-slow" />Bingo
+          </div>
+        <div className="flex space-x-4">
+          <Button variant="destructive" onClick={onNewGame}>
+            <RotateCcw/>Nueva Partida
+          </Button>
+          <Button variant="default" asChild>
+            <Link href="/tickets">
+              <Printer/>Tickets
+            </Link>
+          </Button>
+          <ModeToggle />
+        </div>
       </div>
     </header>
   )
